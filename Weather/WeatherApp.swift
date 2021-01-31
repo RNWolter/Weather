5a3b947b279d2d5683723f28ef204785cdca12cf
Rick <rnwolter@icloud.com>
2021-01-30 22:12:04 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Rick W. on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
